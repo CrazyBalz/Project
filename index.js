397994c7121ef9a0c0875030dfd755a70252f504
crazybalz <iansteiger07@gmail.com>
2023-11-15 10:26:57 -0500

    added winning constant

--- index.js
+++ index.js
@@ -5,18 +5,15 @@ function getBlock(blockId) {
 }
 
 function reset() {
-  // for (let i = 0; i < 9; i++) {
-  //   getBlock(`r${Math.floor(i / 3)}c${i}`).textContent = ""
-  // }
-  getBlock('r1c1').textContent = ""
-  getBlock('r1c2').textContent = ""
-  getBlock('r1c3').textContent = ""
-  getBlock('r2c1').textContent = ""
-  getBlock('r2c2').textContent = ""
-  getBlock('r2c3').textContent = ""
-  getBlock('r3c1').textContent = ""
-  getBlock('r3c2').textContent = ""
-  getBlock('r3c3').textContent = ""
+    getBlock('r1c1').textContent = ""
+    getBlock('r1c2').textContent = ""
+    getBlock('r1c3').textContent = ""
+    getBlock('r2c1').textContent = ""
+    getBlock('r2c2').textContent = ""
+    getBlock('r2c3').textContent = ""
+    getBlock('r3c1').textContent = ""
+    getBlock('r3c2').textContent = ""
+    getBlock('r3c3').textContent = ""
 }
 
 function onClick(input) {
@@ -36,5 +33,20 @@ function onClick(input) {
         block.textContent = 'O';
         turn = 'x';
     }
+    checkWinner();
+}
 
+function checkWinner() {
+    const winners = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 1, 2], [3, 4, 5], [6, 7, 8], [6, 4, 2], [0, 4, 8]]
+    let blocks = [];
+    blocks = [...blocks, getBlock('r1c1').textContent]
+    blocks = [...blocks, getBlock('r1c2').textContent]
+    blocks = [...blocks, getBlock('r1c3').textContent]
+    blocks = [...blocks, getBlock('r2c1').textContent]
+    blocks = [...blocks, getBlock('r2c2').textContent]
+    blocks = [...blocks, getBlock('r2c3').textContent]
+    blocks = [...blocks, getBlock('r3c1').textContent]
+    blocks = [...blocks, getBlock('r3c2').textContent]
+    blocks = [...blocks, getBlock('r3c3').textContent]
+    console.log(blocks)
 }
